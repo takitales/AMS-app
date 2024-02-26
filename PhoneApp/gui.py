daad048f4a64ca5bd324540c5bd99cab9fd5fe3e
Daryn Jacobson <darynjacobson3@gmail.com>
2024-02-25 23:13:11 -0600

    Added new gui and main files

--- PhoneApp/gui.py
+++ PhoneApp/gui.py
@@ -8,7 +8,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.core.window import Window
-from wificonnection import WiFiCommunicator
+from PhoneApp.wificonnection import WiFiCommunicator
 
 #create colors for button
 grey = [1,1,1,1]
---
+++ PhoneApp/main.py
@@ -0,0 +1,6 @@
+from PhoneApp.gui import PhoneApp
+from PhoneApp.wificonnection import WiFiCommunicator
+
+if __name__ == "__main__":
+    app = PhoneApp()
+    app.run()
