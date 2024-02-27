6574115c5abb118d0c5a4c1af1e60cb32c84d32c
Daryn Jacobson <darynjacobson3@gmail.com>
2024-02-27 10:59:32 -0600

    added comments and stuff

--- PhoneApp/gui.py
+++ PhoneApp/gui.py
@@ -8,7 +8,6 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.core.window import Window
-from PhoneApp.wificonnection import WiFiCommunicator
 
 #create colors for button
 grey = [1,1,1,1]
