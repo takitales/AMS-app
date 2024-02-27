d1965426430291e1ee41cf50f0c8bd896f629e77
takitales <145528046+takitales@users.noreply.github.com>
2024-02-26 18:31:13 -0600

    changed code for wificonnection

--- PhoneApp/wificonnection.py
+++ PhoneApp/wificonnection.py
@@ -1,11 +1,59 @@
-import socket
+# import module
+import os
 
-HOST = '192.168.1.100'  # IP address of your microcontroller
-PORT = 1234             # Port number you're using for communication
+# function to establish a new connection
+def createNewConnection(name, SSID, password):
+	config = """<?xml version=\"1.0\"?>
+<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+	<name>"""+name+"""</name>
+	<SSIDConfig>
+		<SSID>
+			<name>"""+SSID+"""</name>
+		</SSID>
+	</SSIDConfig>
+	<connectionType>ESS</connectionType>
+	<connectionMode>auto</connectionMode>
+	<MSM>
+		<security>
+			<authEncryption>
+				<authentication>WPA2PSK</authentication>
+				<encryption>AES</encryption>
+				<useOneX>false</useOneX>
+			</authEncryption>
+			<sharedKey>
+				<keyType>passPhrase</keyType>
+				<protected>false</protected>
+				<keyMaterial>"""+password+"""</keyMaterial>
+			</sharedKey>
+		</security>
+	</MSM>
+</WLANProfile>"""
+	command = "netsh wlan add profile filename=\""+name+".xml\""+" interface=Wi-Fi"
+	with open(name+".xml", 'w') as file:
+		file.write(config)
+	os.system(command)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, microcontroller!')
-    data = s.recv(1024)
+# function to connect to a network 
+def connect(name, SSID):
+	command = "netsh wlan connect name=\""+name+"\" ssid=\""+SSID+"\" interface=Wi-Fi"
+	os.system(command)
 
-print('Received', repr(data))
+# function to display avavilabe Wifi networks 
+def displayAvailableNetworks():
+	command = "netsh wlan show networks interface=Wi-Fi"
+	os.system(command)
+
+
+# display available netwroks
+displayAvailableNetworks()
+
+# input wifi name and password
+name = input("Name of Wi-Fi: ")
+password = input("Password: ")
+
+# establish new connection
+createNewConnection(name, name, password)
+
+# connect to the wifi network
+connect(name, name)
+print("If you aren't connected to this network, try connecting with the correct password!")
