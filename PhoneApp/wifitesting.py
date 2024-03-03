b96a790a56fad9d77b528e58f15a3bf21c1ebb7c
Daryn Jacobson <darynjacobson3@gmail.com>
2024-03-03 01:07:13 -0600

    fixed network hopefully

--- PhoneApp/wifitesting.py
+++ PhoneApp/wifitesting.py
@@ -5,10 +5,8 @@ ESP_IP = '192.168.1.12'  # Replace with the IP address of your ESP32
 ESP_PORT = 1234  # Choose a port number
 
 class wificommands():
-        guiCommand = ''
         def send_command(command):
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         s.connect((ESP_IP, ESP_PORT))
                         s.sendall(command.encode())
                         data = s.recv(1024)
-                        print('Received: ' + command)
