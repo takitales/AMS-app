ed1b0aa6d6640ad89cde01c8effac2d45aad05a2
Daryn Jacobson <darynjacobson3@gmail.com>
2024-09-06 23:09:22 -0500

    added back the imports to hope it works on the phone because it crashes the app

--- PhoneApp/gui.py
+++ PhoneApp/gui.py
@@ -1,11 +1,20 @@
 import kivy
+import time
 import asyncio
 import threading
+import PhoneApp.wifitesting as wifitesting
+import random
+
 from bleak import BleakClient, BleakError, BleakScanner
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.graphics import Color, Ellipse, Line
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
 from kivy.uix.textinput import TextInput
 from kivy.clock import Clock
 
